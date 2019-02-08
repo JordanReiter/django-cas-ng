98bdf160e4a81728e939cee210961f1c3225c168
Jordan Reiter <jordanreiter@gmail.com>
2019-02-08 14:46:59 -0500

    Allow CAS server URL to be dynamic.
    There are cases where a CAS server might have multiple domains, or where for some other reason you want to redirect users to a different login server based on either something about the current request (a cookie, the hostname, etc) or the final destination specified in the service URL (for example, due to a legacy system you might have one SSO for the main site and another for /admin/).
    
    The addition of get_server_url allows this flexibility. Developers can then specify CAS_SERVER_URL as a callable that responds differently based on input:
    
        DEFAULT_CAS_SERVER_URL = 'https://login.example.com/cas'
        def get_cas_server_url(service_url=None, request=None):
            if not request:
                return DEFAULT_CAS_SERVER_URL
            domain = '.'.join(request.get_host().split('.')[-2:])
            return 'https://login.{}/cas'.format(domain)
        CAS_SERVER_URL = get_cas_server_url

--- django_cas_ng/utils.py
+++ django_cas_ng/utils.py
@@ -60,6 +60,16 @@ def get_service_url(request, redirect_to=None):
         })
     return service
 
+def get_server_url(service_url=None, request=None):
+    """
+    Generates the server URL used by the CAS client.
+    If CAS_SERVER_URL is a callable, it creates the URL
+    based on the service URL and request object.
+    Otherwise, it just returns CAS_SERVER_URL
+    """
+    if callable(django_settings.CAS_SERVER_URL):
+        return django_settings.CAS_SERVER_URL(service_url, request)
+    return django_settings.CAS_SERVER_URL
 
 def get_cas_client(service_url=None, request=None):
     """
@@ -67,7 +77,7 @@ def get_cas_client(service_url=None, request=None):
     the CAS_* settigs
     """
     # Handle CAS_SERVER_URL without protocol and hostname
-    server_url = django_settings.CAS_SERVER_URL
+    server_url = get_server_url(service_url=service_url, request=request)
     if server_url and request and server_url.startswith('/'):
         scheme = request.META.get("X-Forwarded-Proto", request.scheme)
         server_url = scheme + "://" + request.META['HTTP_HOST'] + server_url
